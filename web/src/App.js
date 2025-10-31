8d8d395fc157c744cfd28caa6a7fdfe48790798a
Banjo <timothybanjo42@gmail.com>
2025-10-31 06:46:27 +0300

    fix: add required CRA files (public/index.html + src/App.js)

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TaskPesa</h1>
+        <p>Earn by completing tasks!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
